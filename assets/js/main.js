c31619225fbb93772ace4042d3c0c750214111cf
dustin-chan-89 <busterdust@gmail.com>
2016-10-23 20:02:41 -0700

    carousel in a good state

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,31 +5,92 @@
 */
 
 (function($) {
-	$(document).ready(function() {
-		$('.baby-images').slick({
-			dots: true,
-		  infinite: true,
-		  speed: 300,
-		  slidesToShow: 1,
-		  centerMode: true,
-		  variableWidth: true,
-			autoplay: true,
-			autoplaySpeed: 2000
-		});
+	var babyImages = 	( '<img id="carousel-image" src="images/carousel/Babies_01.jpg"/>'
+										);
 
-		$('.baby-images').hide();
+	var childrenImages = 	( '<img id="carousel-image" src="images/carousel/Children_02.jpg"/>' +
+											'<img id="carousel-image" src="images/carousel/Children_03.jpg"/>' +
+											'<img id="carousel-image" src="images/carousel/Children_04.jpg"/>' +
+											'<img id="carousel-image" src="images/carousel/Children_05.jpg"/>' +
+											'<img id="carousel-image" src="images/carousel/Children_06.jpg"/>'
+										);
+
+	var familyImages = ('<img id="carousel-image" src="images/carousel/Families_01.jpg"/>' +
+											'<img id="carousel-image" src="images/carousel/Families_02.jpg"/>' +
+											'<img id="carousel-image" src="images/carousel/Families_03.jpg"/>' +
+											'<img id="carousel-image" src="images/carousel/Families_04.jpg"/>' +
+											'<img id="carousel-image" src="images/carousel/Families_05.jpg"/>' +
+											'<img id="carousel-image" src="images/carousel/Families_06.jpg"/>' +
+											'<img id="carousel-image" src="images/carousel/Families_07.jpg"/>' +
+											'<img id="carousel-image" src="images/carousel/Families_08.jpg"/>' +
+											'<img id="carousel-image" src="images/carousel/Families_09.jpg"/>' +
+											'<img id="carousel-image" src="images/carousel/Families_10.jpg"/>' +
+											'<img id="carousel-image" src="images/carousel/Families_11.jpg"/>' +
+											'<img id="carousel-image" src="images/carousel/Families_12.jpg"/>' +
+											'<img id="carousel-image" src="images/carousel/Families_13.jpg"/>' +
+											'<img id="carousel-image" src="images/carousel/Families_14.jpg"/>' +
+											'<img id="carousel-image" src="images/carousel/Families_15.jpg"/>'
+										);
+	var couplesImages = 	( '<img id="carousel-image" src="images/carousel/Couples_01.jpg"/>' +
+													'<img id="carousel-image" src="images/carousel/Couples_02.jpg"/>' +
+													'<img id="carousel-image" src="images/carousel/Couples_03.jpg"/>' +
+													'<img id="carousel-image" src="images/carousel/Couples_04.jpg"/>' +
+													'<img id="carousel-image" src="images/carousel/Couples_05.jpg"/>'
+												);
+	$(document).ready(function() {
+		$('.carousel-images').hide();
 	});
 
 	$(document).on('click', '#baby-images-link', function(e) {
 		e.preventDefault();
-		$('.gallery').fadeOut();
-		$('.baby-images').fadeIn();
-		$('.carousel-header').fadeIn();
+		initializeCarousel(babyImages);
 	});
+
+	$(document).on('click', '#children-images-link', function(e) {
+		e.preventDefault();
+		initializeCarousel(childrenImages);
+		$('.carousel-header').html('Children<button id="carousel-back-button">Go Back</button>');
+
+	});
+
+	$(document).on('click', '#couple-images-link', function(e) {
+		e.preventDefault();
+		initializeCarousel(couplesImages);
+		$('.carousel-header').html('Couples<button id="carousel-back-button">Go Back</button>');
+	});
+
+	$(document).on('click', '#family-images-link', function(e) {
+		e.preventDefault();
+		initializeCarousel(familyImages);
+		$('.carousel-header').html('Families<button id="carousel-back-button">Go Back</button>');
+
+	});
+
+	function initializeCarousel(images) {
+		$('.gallery').fadeOut();
+		$('.carousel-images').html(images);
+		$('.carousel-images').slick({
+			dots: true,
+			infinite: true,
+			speed: 300,
+			slidesToShow: 1,
+			centerMode: true,
+			variableWidth: true,
+			autoplay: true,
+			autoplaySpeed: 2000
+		});
+		setTimeout(function() {
+			$('.carousel-images').fadeIn();
+			$('.carousel-header').fadeIn();
+		}, 500);
+	}
 
 	$(document).on('click', '#carousel-back-button', function(e) {
 		e.preventDefault();
-		$('.baby-images').fadeOut();
+		$('.carousel-header').hide();
+		$('.carousel-images').fadeOut();
+		$('.carousel-images').slick('unslick');
+		$('.carousel-images').empty();
 		$('.carousel-header').fadeOut();
 		$('.gallery').fadeIn();
 	});
